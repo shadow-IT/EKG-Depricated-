6afe52fd89c4bd7a7886d82bb15c33a5009202af
lindlaaa <develop.lindley@gmail.com>
2018-09-28 09:49:35 -0500

    Updated API to call redis. Added basic functionality to redis.

--- History/server.js
+++ History/server.js
@@ -1,6 +1,26 @@
 var express = require('express');
-var Api = require('./api/Api');
+var bodyParser = require('body-parser')
+var redis = require('redis');
+
 var app = express();
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	extended: true
+})); 
+var client = redis.createClient('https://redis');
+
+client.on('connect', function() {
+	console.log('Redis client connected');
+});
+
+client.on('error', function (err) {
+	console.log('Something went wrong ' + err);
+});
+
+// REMOVEME
+client.set('testName', true, redis.print)
+client.set('count', 1, redis.print);
 
 
 const PORT = 3001
@@ -10,7 +30,45 @@ app.get('/', function(req, res){
 });
 
 app.get('/api', function(req, res) {
-	res.send(Api())
+	client.dbsize(function(error, result) {
+		console.log('result:',result)
+		res.send({result})
+	})
+})
+
+app.get('/api/:serviceName', function(req, res) {
+	const serviceName = req.params.serviceName
+	client.get(serviceName, function (error, result) {
+		console.log('client.get,', serviceName)
+		if (error) {
+			console.log(error);
+			throw error;
+		}
+		console.log('GET result ->' + result);
+		res.send(result)
+	});
+})
+
+app.post('/api/:serviceName', function(req, res) {
+	const serviceName = req.params.serviceName
+	const url = req.body.url
+
+	if(!serviceName || !url){
+		res.statusCode = 400
+		console.log('A param is missing')
+		res.send('A required param is missing.')
+		return
+	}
+
+	console.log(serviceName)
+	client.set(serviceName, url, function (error, result) {
+		console.log('client.set ',serviceName, ' url:', url)
+		if (error) {
+			console.log(error);
+			throw error;
+		}
+		res.send(result)
+	});
 })
 
 console.log('Listening on port:',PORT)
