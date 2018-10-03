dacb517f64fcd0ee8c25800071b390ea163047ff
davidfenton <davidfenton@users.noreply.github.com>
2018-10-02 21:29:08 -0500

    Adding font awesome dependencies and testing the React component for them. Interesting way to specify the specific icon style.

--- Monitor/pages/index.js
+++ Monitor/pages/index.js
@@ -1,9 +1,20 @@
 import fetch from 'isomorphic-unfetch'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as fasHeart, faHeartbeat as fasHeartbeat } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fasHeart, fasHeartbeat, farHeart)
+
 const Index = (props) => (
 	<div>
 		<h1>Refresh test</h1>
 		<p>{props.result}</p>
+
+		<FontAwesomeIcon icon={["fas", "heart"]}/>
+		<FontAwesomeIcon icon={["far", "heart"]}/>
+		<FontAwesomeIcon icon={["fas", "heartbeat"]}/>
 	</div>
 )
 
