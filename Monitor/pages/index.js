23d1c9717b8cbd438af40f85632c017867148730
davidfenton <davidfenton@users.noreply.github.com>
2018-10-15 20:03:43 -0500

    Creating a simple text health component.

--- Monitor/pages/index.js
+++ Monitor/pages/index.js
@@ -1,31 +1,11 @@
 import fetch from 'isomorphic-unfetch'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart as fasHeart, faHeartbeat as fasHeartbeat } from '@fortawesome/free-solid-svg-icons'
-import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
-
 import HealthText from "../components/health/HealthText";
-
-library.add(fasHeart, fasHeartbeat, farHeart)
 
 const Index = (props) => (
 	<div>
 		<h1>Refresh test</h1>
-		{/* <ul>
-		{props.results.map(result => {
-			let color = result.status === 200
-				? {color: 'green'}
-				: {color: 'red'}
-			return <li style={color}>{result.name}</li>
-
-		})}
-		</ul> */}
 		{props.results.map(result => <HealthText key={result.name} alive={result.status === 200}/>)}
-
-		<FontAwesomeIcon icon={["fas", "heart"]}/>
-		<FontAwesomeIcon icon={["far", "heart"]}/>
-		<FontAwesomeIcon icon={["fas", "heartbeat"]}/>
 	</div>
 )
 
