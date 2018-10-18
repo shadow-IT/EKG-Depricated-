324e067aa43a90641b14367e59c79132390c659f
lindlaaa <develop.lindley@gmail.com>
2018-10-18 16:04:00 -0500

    Added axios middleware for logging performance

--- Commuter/commuter.js
+++ Commuter/commuter.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express()
-const fetch = require('isomorphic-unfetch')
+const axios = require('axios')
+const {performance} = require('perf_hooks');
+
+axios.interceptors.request.use(function (config) {
+
+	config.metadata = { startTime: performance.now()}
+	return config;
+})
+
+axios.interceptors.response.use(function (response) {
+	response.config.metadata.endTime = performance.now()
+	response.duration = response.config.metadata.endTime - response.config.metadata.startTime
+	return response;
+})
+
+axios.interceptors.response.use(function (response) {
+	console.log(response.config.method + ' request to ' + response.config.url + ' took ' + response.duration + 'ms with status ' + response.status)
+	return response;
+})
 
 const port = 3004
 
 app.get('/', function(req, res) {
 	axios.get('https://httpstat.us/200')
 		.then(result => {
-			console.log('result=',result)
 			res.send(result.data)
 		}).catch(error => {
 			console.log(error)
